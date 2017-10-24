7757ae52f2fc431984aeb0c6d4bbe4e0a4e872d5
Prasnal <dorota@lisp.pl>
2017-10-24 19:39:07 +0200

    password added to config file, added quote functionality

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.request import Request
 import random
 
-def random_quota():
+def random_quote():
     user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46'
     conn=urlopen(Request('https://www.inc.com/dave-kerpen/365-quotes-to-inspire-you-in-2014.html',data=None, headers={'User-Agent': user_agent}))
     html_content = conn.read()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from functions import random_quote
 import fbchat
 import cat
 from fbchat.models import *
 import random
+import config
 
 
 class EchoBot(fbchat.Client):
@@ -9,30 +11,33 @@ class EchoBot(fbchat.Client):
     def __init__(self,email, password, debug=True, user_agent=None):
         fbchat.Client.__init__(self,email, password, debug, user_agent)
 
+    def send_quote(self,thread_id,thread_type):
+        quote=random_quote()
+        self.sendMessage(quote, thread_id=thread_id, thread_type=thread_type)
+
     def send_cat(self,thread_id,thread_type):
         img=cat.getCat(directory=None,filename='cat',format='jpg')
-        #self.sendLocalImage(img,'cat',thread_id,thread_type)
         self.sendLocalImage(img, message = None,thread_id=thread_id,thread_type=thread_type)
 
 
     def check_msg(self,msg,thread_id,thread_type):
         print(msg)
-        if (msg=='!kot'):
+        if (msg=='!cat'):
             self.send_cat(thread_id,thread_type)
+        elif (msg=='!quote'):
+            self.send_quote(thread_id,thread_type)
 
 
     def onMessage(self, mid, author_id, message, message_object, thread_id, thread_type, ts, metadata,msg):
-        print(author_id)
-        print(thread_id)
-        print(thread_type)
-        print(message)
+        #print(author_id)
+        #print(thread_id)
+        #print(thread_type)
+        #print(message)
         self.friendConnect(author_id)
         self.check_msg(message,thread_id,thread_type)
-        print(self.fetchThreadList())
+        #print(self.fetchThreadList())
 
 
-    def onInbox(self, unseen, unread, recent_unread, msg):
-        print("test")
 
-bot = EchoBot("<email>", "<password>",None,3)
+bot = EchoBot(config.username, config.password,None,3)
 bot.listen()
